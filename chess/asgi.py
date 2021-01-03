31fb441b7de4541f3fe37a4c999dd8c3997d5413
kevin <kevinjoseph61@gmail.com>
2021-01-04 00:03:26 +0530

    channels error

--- chess/asgi.py
+++ chess/asgi.py
@@ -8,14 +8,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.conf import settings
-from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
 from asgi_middleware_static_file import ASGIMiddlewareStaticFile
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chess.settings')
 
-application = get_asgi_application()
+django.setup()
+
+application = get_default_application()
 
 application = ASGIMiddlewareStaticFile(
     application, static_url=settings.STATIC_URL, static_paths=[settings.STATIC_ROOT]
