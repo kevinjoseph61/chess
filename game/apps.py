711149bacd7ff0ac316a87a0b8e6b049a411fa07
Kevin Joseph <kevinjoseph61@gmail.com>
2026-05-25 00:12:50 +0530

    Track quantized model, fix apps.py startup guard

--- game/apps.py
+++ game/apps.py
@@ -3,10 +3,13 @@ from django.apps import AppConfig
 class GameConfig(AppConfig):
     name = 'game'
     def ready(self):
-        from .models import Game
-        g = Game.objects.all()
-        for i in g:
-            i.opponent_online = i.owner_online = False
-            i.save()
-        print("Resetting online statuses")
+        try:
+            from .models import Game
+            g = Game.objects.all()
+            for i in g:
+                i.opponent_online = i.owner_online = False
+                i.save()
+            print("Resetting online statuses")
+        except Exception:
+            pass
 
